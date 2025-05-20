7e38f760f691d5993f6194ad6fa99f18a251762e
Letho1972 <ericmarescq@gmail.com>
2025-05-20 15:23:36 +0200

    version 0

---
+++ mon-api-backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/voisin', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connecté'))
+  .catch(err => console.error(err));
+
+app.use('/api', authRoutes);
+
+app.listen(3000, () => {
+  console.log('Serveur démarré sur le port 3000');
+});
